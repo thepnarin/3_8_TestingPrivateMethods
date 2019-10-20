058aeb1267bbb9cbbde1bb17ee8aae8187413f37
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 13:54:01 +0700

    TestingPrivateMethods

---
+++ 3_8_TestingPrivateMethods/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_8_TestingPrivateMethods.Fundamentals
+{
+    public class ErrorLogger
+    {
+        public string LastError { get; set; }
+
+        public event EventHandler<Guid> ErrorLogged;
+        private Guid _errorId;
+
+        public void Log(string error)
+        {
+            if (String.IsNullOrWhiteSpace(error))
+                throw new ArgumentNullException();
+
+            LastError = error;
+
+            // Write the log to a storage
+            // ...
+
+            OnErrorLogged(Guid.NewGuid());
+        }
+        protected virtual void OnErrorLogged(Guid errorId)
+        {
+            ErrorLogged?.Invoke(this, errorId);
+        }
+    }
+}
